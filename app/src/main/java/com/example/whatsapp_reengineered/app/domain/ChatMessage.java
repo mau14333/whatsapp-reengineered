b215ce89fc79aa8c9a69f557a046fe3c7c716534
Michael Maurer <maurerm3141@gmail.com>
2015-03-10 09:36:37 +0100

    L O M B O K O M B O K

--- app/src/main/java/com/example/whatsapp_reengineered/app/domain/ChatMessage.java
+++ app/src/main/java/com/example/whatsapp_reengineered/app/domain/ChatMessage.java
@@ -1,10 +1,15 @@
 package com.example.whatsapp_reengineered.app.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Michi on 10.03.2015.
  */
 public class ChatMessage {
 
+    @Getter
+    @Setter
     private String text;
 
     public ChatMessage(String text) {
